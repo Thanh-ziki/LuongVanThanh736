faa52a1d5ccd8ef771dc55b029696c85fab54026
Thành Lương <you@example.comthanh.luongvan2k@gmail.com>
2023-01-04 11:29:30 +0700

    first_commit_LVT736

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LuongVanThanh736.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
